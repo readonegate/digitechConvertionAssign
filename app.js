c48cd8162ade3b1d7d8bbebbbf14ce0508976c76
readonegate <ridwan.gate@gmail.com>
2024-04-19 22:32:03 +0700

    refactor model algoritm

--- app.js
+++ app.js
@@ -1,63 +1,144 @@
 function convert() {
     const numberInput = document.getElementById("number-input").value;
-    const conversionType = document.getElementById("conversion-type").value;
+    const conversionType = document.getElementById("select3-1").value;
     let result;
     let basis;
+    let regex = /^[A-Za-z]+$/;
+    let regex2 = /^[0-1]+$/;
+    let regex3 = /^[0-7]+$/;
+    let regex4 = /^[0-9]+[A-Fa-f]+$/;
+    let regex5 = /^[0-9]+$/;
+
 
     switch (conversionType) {
         case "binary-to-decimal":
-            result = parseInt(numberInput, 2);
-            basis = '10';
+            if(regex2.test(numberInput)){
+                result = parseInt(numberInput, 2);
+                basis = '10';
+            }else{
+                window.alert(`angka yang di inputkan hanya 1 dan 0`);
+                const element = document.getElementById("output");
+                element.remove();
+            }
             break;
         case "decimal-to-binary":
-            result = Number(numberInput).toString(2);
-            basis = '2';
+            if(regex.test(numberInput)){
+                result = Number(numberInput).toString(2);
+                basis = '2';
+            }else{
+                window.alert(`Hanya angka yang di inputkan`);
+                const element = document.getElementById("output");
+                element.remove();
+            }
             break;
         case "binary-to-hex":
-            const decimalEquivalent = parseInt(numberInput, 2);
-            result = decimalEquivalent.toString(16).toUpperCase();
-            basis = '16';
+            if(regex2.test(numberInput)){
+                const decimalEquivalent = parseInt(numberInput, 2);
+                result = decimalEquivalent.toString(16).toUpperCase();
+                basis = '16';
+            }else{
+                window.alert(`angka yang di inputkan hanya 1 dan 0`);
+                const element = document.getElementById("output");
+                element.remove();
+            }
             break;
         case "hex-to-binary":
-            const decimalEquivalent2 = parseInt(numberInput, 16);
-            result = decimalEquivalent2.toString(2);
-            basis = '2';
+            if(regex4.test(numberInput) || regex5.test(numberInput)){
+                const decimalEquivalent2 = parseInt(numberInput, 16);
+                result = decimalEquivalent2.toString(2);
+                basis = '2';
+            }else{
+                window.alert(`angka yang di inputkan hanya 1 - 9 dan huruf A - F`);
+                const element = document.getElementById("output");
+                element.remove();
+            }
+            
             break;
         case "hex-to-octal":
-            const decimalEquivalent4 = parseInt(numberInput, 16);
-            result = decimalEquivalent4.toString(8);
-            basis = '8';
+            if(regex4.test(numberInput) || regex5.test(numberInput)){
+                const decimalEquivalent4 = parseInt(numberInput, 16);
+                result = decimalEquivalent4.toString(8);
+                basis = '8';
+            }else{
+                window.alert(`angka yang di inputkan hanya 1 - 9 dan huruf A - F`);
+                const element = document.getElementById("output");
+                element.remove();
+            }
+            
             break;
         case "decimal-to-hex":
-            result = Number(numberInput).toString(16).toUpperCase();
-            basis = '16';
+            if(regex.test(numberInput)){
+                result = Number(numberInput).toString(16).toUpperCase();
+                basis = '16';
+            }else{
+                window.alert(`Hanya angka yang di inputkan`);
+                const element = document.getElementById("output");
+                element.remove();
+            }
             break;
         case "hex-to-decimal":
-            result = parseInt(numberInput, 16);
-            basis = '10';
+            if(regex4.test(numberInput) || regex5.test(numberInput)){
+                result = parseInt(numberInput, 16);
+                basis = '10';
+            }else{
+                window.alert(`angka yang di inputkan hanya 1 - 9 dan huruf A - F`);
+                const element = document.getElementById("output");
+                element.remove();
+            }
+            
             break;
         case "decimal-to-octal":
-            result = Number(numberInput).toString(8);
-            basis = '8';
+            if(regex.test(numberInput)){
+                result = Number(numberInput).toString(8);
+                basis = '8';
+            }else{
+                window.alert(`Hanya angka yang di inputkan`);
+                const element = document.getElementById("output");
+                element.remove();
+            }
             break;
         case "binary-to-octal":
-            const decimalEquivalent3 = parseInt(numberInput, 2);
-            result = decimalEquivalent3.toString(8);
-            basis = '8';
+            if(regex2.test(numberInput)){
+                const decimalEquivalent3 = parseInt(numberInput, 2);
+                result = decimalEquivalent3.toString(8);
+                basis = '8';
+            }else{
+                window.alert(`angka yang di inputkan hanya 1 dan 0`);
+                const element = document.getElementById("output");
+                element.remove();
+            }
             break;
         case "octal-to-binary":
-            const decimalEquivalent5 = parseInt(numberInput, 8);
-            result = decimalEquivalent5.toString(2);
-            basis = '2';
+            if(regex3.test(numberInput)){
+                const decimalEquivalent5 = parseInt(numberInput, 8);
+                result = decimalEquivalent5.toString(2);
+                basis = '2';
+            }else{
+                window.alert(`angka yang di inputkan hanya 1 - 7`);
+                const element = document.getElementById("output");
+                element.remove();
+            }
             break;
         case "octal-to-hex":
-            const decimalEquivalent6 = parseInt(numberInput, 8);
-            result = decimalEquivalent6.toString(16).toUpperCase();
-            basis = '16';
+            if(regex3.test(numberInput)){
+                const decimalEquivalent6 = parseInt(numberInput, 8);
+                result = decimalEquivalent6.toString(16).toUpperCase();
+                basis = '16';
+            }else{
+                window.alert(`angka yang di inputkan hanya 1 - 7`);
+                const element = document.getElementById("output");
+                element.remove();
+            }
             break;
         case "octal-to-decimal":
-            result = parseInt(numberInput, 8);
-            basis = '10';
+            if(regex3.test(numberInput)){
+                result = parseInt(numberInput, 8);
+                basis = '10';
+            }else{
+                window.alert(`angka yang di inputkan hanya 1 - 7`);
+                const element = document.getElementById("output");
+                element.remove();
+            }
             break;
         default:
             result = "Invalid Conversion Type";
@@ -68,8 +149,39 @@ function convert() {
 }
 
 
-
 function reset(){
     location.reload();
 return false;
 }
+
+
+
+function ConvertionSelect(convert, selectId) {
+    var select3 = document.getElementById(selectId);
+
+    // Kosongkan select2
+    select3.innerHTML = '';
+
+    if (convert === 'decimal') {
+      var decimalVarians = ['decimal-to-binary', 'decimal-to-octal', 'decimal-to-hex'];
+      convertionOptions(select3, decimalVarians);
+    } else if (convert === 'biner') {
+      var binerVarians = ['binary-to-decimal', 'binary-to-octal', 'binary-to-hex'];
+      convertionOptions(select3, binerVarians);
+    } else if (convert === 'octal') {
+      var octalVarians = ['octal-to-decimal', 'octal-to-binary', 'octal-to-hex'];
+      convertionOptions(select3, octalVarians);
+    }else if (convert === 'hex') {
+      var hexVarians = ['hex-to-decimal', 'hex-to-binary', 'hex-to-octal'];
+      convertionOptions(select3, hexVarians);
+    }
+  }
+
+  function convertionOptions(selectElement, optionsArray) {
+    optionsArray.forEach(function(option) {
+      var optionElement = document.createElement('option');
+      optionElement.textContent = option;
+      optionElement.value = option;
+      selectElement.appendChild(optionElement);
+    });
+  }
